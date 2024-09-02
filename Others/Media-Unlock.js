578a93bbf3c54dafcaa35f51b01652432af2840b
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-09-03 02:25:02 +0800

    Update Media-Unlock.js

--- Others/Media-Unlock.js
+++ Others/Media-Unlock.js
@@ -55,7 +55,7 @@ let args = getArgs();
   let youtubeResult = await check_youtube_premium();
  
   let disney_result = formatDisneyPlusResult(status, region);
-  let content = ` ${netflixResult} ${disney_result} ${youtubeResult}`;
+  let content = ` ${netflixResult} ${youtubeResult} ${disney_result}`;
   
   let traceData = await getTraceData();
   let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
@@ -132,13 +132,13 @@ async function check_youtube_premium() {
   await inner_check()
     .then((code) => {
       if (code === 'Not Available') {
-        youtube_check_result += '\u2612 |';
+        youtube_check_result += '\u2612';
       } else {
-        youtube_check_result += "\u2611" + code.toUpperCase() + ' |';
+        youtube_check_result += "\u2611" + code.toUpperCase() ;
       }
     })
     .catch(() => {
-      youtube_check_result += 'N/A |';
+      youtube_check_result += 'N/A';
     });
 
   return youtube_check_result;
@@ -190,7 +190,7 @@ async function check_netflix() {
       if (code === 'Not Found') {
         return inner_check(80018499);
       }
-      netflix_check_result += '\u2611' + code.toUpperCase() ;
+      netflix_check_result += '\u2611' + code.toUpperCase() + ' |';
       return Promise.reject('BreakSignal');
     })
     .then((code) => {
@@ -198,7 +198,7 @@ async function check_netflix() {
         return Promise.reject('Not Available');
       }
 
-      netflix_check_result += '⚠' + code.toUpperCase() ;
+      netflix_check_result += '⚠' + code.toUpperCase() + ' |';
       return Promise.reject('BreakSignal');
     })
     .catch((error) => {
@@ -206,10 +206,10 @@ async function check_netflix() {
         return;
       }
       if (error === 'Not Available') {
-        netflix_check_result += '\u2612';
+        netflix_check_result += '\u2612 |';
         return;
       }
-      netflix_check_result += 'N/A';
+      netflix_check_result += 'N/A |';
     });
 
   return netflix_check_result;
