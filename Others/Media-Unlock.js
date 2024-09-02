6b1fc658d5f2f4b8228fad052303cf445134dd9f
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-09-03 04:08:58 +0800

    Update Media-Unlock.js

--- Others/Media-Unlock.js
+++ Others/Media-Unlock.js
@@ -58,8 +58,8 @@ let args = getArgs();
   let traceData = await getTraceData();
   let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
  
-  let content = `${youtubeResult} ${netflixResult}\n${gptSupportStatus}${flagEmoji(traceData.loc)}${traceData.loc} ${disney_result}`;
-
+  let content = `${youtubeResult} ${netflixResult}\n${gptSupportStatus}${traceData.loc.padEnd(3)} ${disney_result}`;
+  
   let log = `${hour}:${minutes}.${now.getMilliseconds()} 解鎖檢測完成：${content}`;
   console.log(log);
 
@@ -77,20 +77,12 @@ function getArgs() {
   );
 }
 
-function flagEmoji(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
-
 function formatDisneyPlusResult(status, region) {
   switch (status) {
     case STATUS_COMING:
-      return `| Disney: 即將登陸~ ${flagEmoji(region)}${region.toUpperCase()} `;
+      return`| Disney: 即將登陸~ ${region.toUpperCase()} `;
     case STATUS_AVAILABLE:
-      return `| Disney: \u2611${flagEmoji(region)}${region.toUpperCase()} `;
+      return `| Disney: \u2611${region.toUpperCase()} `;
     case STATUS_NOT_AVAILABLE:
       return `| Disney: \u2612`;
     case STATUS_TIMEOUT:
@@ -140,7 +132,7 @@ async function check_youtube_premium() {
       if (code === 'Not Available') {
         youtube_check_result += '\u2612 |';
       } else {
-        youtube_check_result += "\u2611" + flagEmoji(code) + code.toUpperCase() + ' |';
+        youtube_check_result += "\u2611" + code.toUpperCase() + ' |';
       }
     })
     .catch(() => {
@@ -196,7 +188,7 @@ async function check_netflix() {
       if (code === 'Not Found') {
         return inner_check(80018499);
       }
-      netflix_check_result += '\u2611' + flagEmoji(code) + code.toUpperCase() + ' |';
+      netflix_check_result += '\u2611' + code.toUpperCase() ;
       return Promise.reject('BreakSignal');
     })
     .then((code) => {
@@ -204,7 +196,7 @@ async function check_netflix() {
         return Promise.reject('Not Available');
       }
 
-      netflix_check_result += '⚠' + flagEmoji(code) + code.toUpperCase() + ' |';
+      netflix_check_result += '⚠' + code.toUpperCase() ;
       return Promise.reject('BreakSignal');
     })
     .catch((error) => {
@@ -212,10 +204,10 @@ async function check_netflix() {
         return;
       }
       if (error === 'Not Available') {
-        netflix_check_result += '\u2612 |';
+        netflix_check_result += '\u2612';
         return;
       }
-      netflix_check_result += 'N/A |';
+      netflix_check_result += 'N/A';
     });
 
   return netflix_check_result;
@@ -224,109 +216,156 @@ async function check_netflix() {
 async function testDisneyPlus() {
   try {
     let { region, cnbl } = await Promise.race([testHomePage(), timeout(7000)]);
-    console.log(`Disney+ Region: ${region || 'N/A'}`);
-    console.log(`Disney+ CNBl: ${cnbl}`);
-    if (cnbl === 2) {
+    console.log(`homepage: region=${region}, cnbl=${cnbl}`);
+    let { countryCode, inSupportedLocation } = await Promise.race([getLocationInfo(), timeout(7000)]);
+    console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`);
+
+    region = countryCode ?? region;
+    console.log("region:" + region);
+    // 即將登陸
+    if (inSupportedLocation === false || inSupportedLocation === 'false') {
       return { region, status: STATUS_COMING };
-    }
-    let support = await Promise.race([testVideoPlayback(), timeout(7000)]);
-    console.log(`Disney+ Playback: ${support}`);
-    if (support) {
-      return { region, status: STATUS_AVAILABLE };
     } else {
+      // 支持解鎖
+      return { region, status: STATUS_AVAILABLE };
+    }
+
+  } catch (error) {
+    console.log("error:" + error);
+
+    // 不支持解鎖
+    if (error === 'Not Available') {
+      console.log("不支持");
       return { status: STATUS_NOT_AVAILABLE };
     }
-  } catch (error) {
-    console.log("Error: " + error);
-    return { status: STATUS_TIMEOUT };
+
+    // 檢測超時
+    if (error === 'Timeout') {
+      return { status: STATUS_TIMEOUT };
+    }
+
+    return { status: STATUS_ERROR };
   }
 }
 
-function getFlagEmoji(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
-
-function getTraceData() {
+function getLocationInfo() {
   return new Promise((resolve, reject) => {
-    let option = {
-      url: 'http://ip-api.com/json/?fields=status,message,countryCode,query',
-      headers: REQUEST_HEADERS,
+    let opts = {
+      url: 'https://disney.api.edge.bamgrid.com/graph/v1/device/graphql',
+      headers: {
+        'Accept-Language': 'en',
+        Authorization: 'ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84',
+        'Content-Type': 'application/json',
+        'User-Agent': UA,
+      },
+      body: JSON.stringify({
+        query: 'mutation registerDevice($input: RegisterDeviceInput!) { registerDevice(registerDevice: $input) { grant { grantType assertion } } }',
+        variables: {
+          input: {
+            applicationRuntime: 'chrome',
+            attributes: {
+              browserName: 'chrome',
+              browserVersion: '94.0.4606',
+              manufacturer: 'apple',
+              model: null,
+              operatingSystem: 'macintosh',
+              operatingSystemVersion: '10.15.7',
+              osDeviceIds: [],
+            },
+            deviceFamily: 'browser',
+            deviceLanguage: 'en',
+            deviceProfile: 'macosx',
+          },
+        },
+      }),
     };
-    $httpClient.get(option, (error, response, data) => {
+
+    $httpClient.post(opts, function (error, response, data) {
       if (error) {
-        reject(error);
+        reject('Error');
         return;
       }
 
-      let json = JSON.parse(data);
-      if (json.status === 'fail') {
-        reject(json.message);
+      if (response.status !== 200) {
+        console.log('getLocationInfo: ' + data);
+        reject('Not Available');
         return;
       }
 
-      resolve({ ip: json.query, loc: json.countryCode });
+      data = JSON.parse(data);
+      if (data?.errors) {
+        console.log('getLocationInfo: ' + data);
+        reject('Not Available');
+        return;
+      }
+
+      let {
+        token: { accessToken },
+        session: {
+          inSupportedLocation,
+          location: { countryCode },
+        },
+      } = data?.extensions?.sdk;
+      resolve({ inSupportedLocation, countryCode, accessToken });
     });
   });
 }
 
 function testHomePage() {
   return new Promise((resolve, reject) => {
-    let option = {
+    let opts = {
       url: 'https://www.disneyplus.com/',
-      headers: REQUEST_HEADERS,
+      headers: {
+        'Accept-Language': 'en',
+        'User-Agent': UA,
+      },
     };
-    $httpClient.get(option, function (error, response, data) {
+
+    $httpClient.get(opts, function (error, response, data) {
       if (error) {
         reject('Error');
         return;
       }
-
-      if (response.status !== 200 || data.indexOf('unavailable') !== -1) {
+      if (response.status !== 200 || data.indexOf('Sorry, Disney+ is not available in your region.') !== -1) {
         reject('Not Available');
         return;
       }
 
       let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/);
       if (!match) {
-        resolve({ region: '', cnbl: 0 });
+        resolve({ region: '', cnbl: '' });
         return;
       }
 
-      resolve({ region: match[1], cnbl: parseInt(match[2], 10) });
+      let region = match[1];
+      let cnbl = match[2];
+      resolve({ region, cnbl });
     });
   });
 }
 
-function testVideoPlayback() {
-  return new Promise((resolve, reject) => {
-    let option = {
-      url: 'https://www.disneyplus.com/video/batman',
-      headers: REQUEST_HEADERS,
-    };
-    $httpClient.get(option, function (error, response, data) {
-      if (error) {
-        reject('Error');
-        return;
-      }
-
-      if (response.status !== 200 || data.indexOf('unavailable') !== -1) {
-        reject('Not Available');
-        return;
-      }
-
-      resolve(true);
-    });
-  });
-}
-
-function timeout(delay) {
+function timeout(delay = 5000) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       reject('Timeout');
     }, delay);
+  });
+}
+
+async function getTraceData() {
+  return new Promise((resolve, reject) => {
+    $httpClient.get("http://chat.openai.com/cdn-cgi/trace", function(error, response, data) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      let lines = data.split("\n");
+      let cf = lines.reduce((acc, line) => {
+        let [key, value] = line.split("=");
+        acc[key] = value;
+        return acc;
+      }, {});
+      resolve(cf);
+    });
   });
 }
