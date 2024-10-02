7f780475efb952331b3cf4cb688ad3b26781a089
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-02 23:44:30 +0800

    Update Media-Unlock1.js

--- Others/Media-Unlock1.js
+++ Others/Media-Unlock1.js
@@ -4,7 +4,7 @@
 Surge配置参考注释
  
  ----------------------------------------
- 
+
 [Panel]
 策略面板 = script-name=解鎖檢測,update-interval=7200
 
@@ -17,7 +17,6 @@ Surge配置参考注释
  * title: 面板標題
  * icon: SFSymbols 圖標
  * color：圖標顏色
- 
  */
 
 const STATUS_COMING = 2;
@@ -30,41 +29,41 @@ const REQUEST_HEADERS = {
   'User-Agent': UA,
   'Accept-Language': 'en',
 };
-const SUPPORTED_LOCATIONS = ["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP,\u2009","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
+const SUPPORTED_LOCATIONS = ["T1", "XX", "AL", "DZ", "AD", "AO", "AG", "AR", "AM", "AU", "AT", "AZ", "BS", "BD", "BB", "BE", "BZ", "BJ", "BT", "BA", "BW", "BR", "BG", "BF", "CV", "CA", "CL", "CO", "KM", "CR", "HR", "CY", "DK", "DJ", "DM", "DO", "EC", "SV", "EE", "FJ", "FI", "FR", "GA", "GM", "GE", "DE", "GH", "GR", "GD", "GT", "GN", "GW", "GY", "HT", "HN", "HU", "IS", "IN", "ID", "IQ", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KI", "KW", "KG", "LV", "LB", "LS", "LR", "LI", "LT", "LU", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MR", "MU", "MX", "MC", "MN", "ME", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NZ", "NI", "NE", "NG", "MK", "NO", "OM", "PK", "PW", "PA", "PG", "PE", "PH", "PL", "PT", "QA", "RO", "RW", "KN", "LC", "VC", "WS", "SM", "ST", "SN", "RS", "SC", "SL", "SG", "SK", "SI", "SB", "ZA", "ES", "LK", "SR", "SE", "CH", "TH", "TG", "TO", "TT", "TN", "TR", "TV", "UG", "AE", "US", "UY", "VU", "ZM", "BO", "BN", "CG", "CZ", "VA", "FM", "MD", "PS", "KR", "TW", "TZ", "TL", "GB"];
 const WARP_FEATURES = ["plus", "on"];
 
 let args = getArgs();
 
 (async () => {
-let now = new Date();
-let hour = now.getHours();
-let minutes = now.getMinutes();
-hour = hour > 9 ? hour : "0" + hour;
-minutes = minutes > 9 ? minutes : "0" + minutes;
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  hour = hour > 9 ? hour : "0" + hour;
+  minutes = minutes > 9 ? minutes : "0" + minutes;
 
-let panel_result = {
-  title: `${args.title} | ${hour}:${minutes}` || `解鎖檢測 | ${hour}:${minutes}`,
-  content: '',
-  icon: args.icon || "eye.slash.circle.fill",
-  "icon-color": args.color || "#ffb621",
-};
+  let panel_result = {
+    title: `${args.title} | ${hour}:${minutes} || 解鎖檢測 | ${hour}:${minutes}`,
+    content: '',
+    icon: args.icon || "eye.slash.circle.fill",
+    "icon-color": args.color || "#ffb621",
+  };
 
-let [{ region, status }] = await Promise.all([testDisneyPlus()]);
-let netflixResult = await check_netflix();
-let youtubeResult = await check_youtube_premium();
+  let [{ region, status }] = await Promise.all([testDisneyPlus()]);
+  let netflixResult = await check_netflix();
+  let youtubeResult = await check_youtube_premium();
 
-let disney_result = formatDisneyPlusResult(status, region);
-let traceData = await getTraceData();
-let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
+  let disney_result = formatDisneyPlusResult(status, region);
+  let traceData = await getTraceData();
+  let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
 
-let content = `${youtubeResult} ${netflixResult}\n${gptSupportStatus}${traceData.loc.padEnd(3)}${disney_result} `;
+  // 調整格式，確保輸出對齊
+  let content = `${youtubeResult} ${netflixResult}\n${gptSupportStatus} ${traceData.loc.padEnd(3)} ${disney_result}`;
+  let log = `${hour}:${minutes}.${now.getMilliseconds()} 解鎖檢測完成：${content}`;
+  console.log(log);
 
-let log = `${hour}:${minutes}.${now.getMilliseconds()} 解鎖檢測完成：${content}`;
-console.log(log);
+  panel_result['content'] = content;
 
-panel_result['content'] = content;
-
-$done(panel_result);
+  $done(panel_result);
 })();
 
 function getArgs() {
@@ -79,15 +78,15 @@ function getArgs() {
 function formatDisneyPlusResult(status, region) {
   switch (status) {
     case STATUS_COMING:
-      return `| Disney: Soon~ ${region.toUpperCase()} `;
+      return `| Disney: Soon~ ${region.toUpperCase()}`;
     case STATUS_AVAILABLE:
-      return `| Disney: \u2611${region.toUpperCase()} `;
+      return `| Disney: \u2611 ${region.toUpperCase()}`;
     case STATUS_NOT_AVAILABLE:
-      return `| Disney: \u2612`;
+      return "| Disney: \u2612";
     case STATUS_TIMEOUT:
-      return `| Disney: N/A `;
+      return "| Disney: N/A";
     default:
-      return `| Disney: 錯誤 `;
+      return "| Disney: 錯誤";
   }
 }
 
@@ -187,7 +186,7 @@ async function check_netflix() {
       if (code === 'Not Found') {
         return inner_check(80018499);
       }
-      netflix_check_result += '\u2611' + code.toUpperCase() ;
+      netflix_check_result += '\u2611' + code.toUpperCase();
       return Promise.reject('BreakSignal');
     })
     .then((code) => {
@@ -195,7 +194,7 @@ async function check_netflix() {
         return Promise.reject('Not Available');
       }
 
-      netflix_check_result += '⚠' + code.toUpperCase() ;
+      netflix_check_result += '⚠' + code.toUpperCase();
       return Promise.reject('BreakSignal');
     })
     .catch((error) => {
@@ -203,142 +202,75 @@ async function check_netflix() {
         return;
       }
       if (error === 'Not Available') {
-        netflix_check_result += '\u2612 ';
+        netflix_check_result += '\u2612';
         return;
       }
       netflix_check_result += 'N/A';
     });
 
-  return netflix_check_result;
+  return netflix_check_result + '  |';
 }
 
 async function testDisneyPlus() {
-  try {
-    let { region, cnbl } = await Promise.race([testHomePage(), timeout(7000)]);
-    console.log(`homepage: region=${region}, cnbl=${cnbl}`);
-    let { countryCode, inSupportedLocation } = await Promise.race([getLocationInfo(), timeout(7000)]);
-    console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`);
-
-    region = countryCode ?? region;
-    console.log("region:" + region);
-    // 即將登陸Soon
-    if (inSupportedLocation === false || inSupportedLocation === 'false') {
-      return { region, status: STATUS_COMING };
-    } else {
-      // 支持解鎖
-      return { region, status: STATUS_AVAILABLE };
-    }
-
-  } catch (error) {
-    console.log("error:" + error);
-
-    // 不支持解鎖
-    if (error === 'Not Available') {
-      console.log("不支持");
-      return { status: STATUS_NOT_AVAILABLE };
-    }
-
-    // 檢測超時
-    if (error === 'Timeout') {
-      return { status: STATUS_TIMEOUT };
-    }
-
-    return { status: STATUS_ERROR };
+  let { region, cnbl } = await Promise.race([getLocation(), timeout(7000)]);
+  if (!region) {
+    return { region: null, status: STATUS_TIMEOUT };
   }
+  let { status } = await Promise.race([testHomePage(), timeout(7000)]);
+  if (status === STATUS_AVAILABLE && cnbl === 1) {
+    status = STATUS_COMING;
+  }
+  return { region, status };
 }
 
-function getLocationInfo() {
+function getLocation() {
   return new Promise((resolve, reject) => {
-    let opts = {
-      url: 'https://disney.api.edge.bamgrid.com/graph/v1/device/graphql',
-      headers: {
-        'Accept-Language': 'en',
-        Authorization: 'ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84',
-        'Content-Type': 'application/json',
-        'User-Agent': UA,
-      },
-      body: JSON.stringify({
-        query: 'mutation registerDevice($input: RegisterDeviceInput!) { registerDevice(registerDevice: $input) { grant { grantType assertion } } }',
-        variables: {
-          input: {
-            applicationRuntime: 'chrome',
-            attributes: {
-              browserName: 'chrome',
-              browserVersion: '94.0.4606',
-              manufacturer: 'apple',
-              model: null,
-              operatingSystem: 'macintosh',
-              operatingSystemVersion: '10.15.7',
-              osDeviceIds: [],
-            },
-            deviceFamily: 'browser',
-            deviceLanguage: 'en',
-            deviceProfile: 'macosx',
-          },
-        },
-      }),
+    let option = {
+      url: 'https://www.disneyplus.com/',
+      headers: REQUEST_HEADERS,
     };
-
-    $httpClient.post(opts, function (error, response, data) {
+    $httpClient.get(option, function (error, response, data) {
       if (error) {
         reject('Error');
         return;
       }
 
-      if (response.status !== 200) {
-        console.log('getLocationInfo: ' + data);
+      if (response.status !== 200 || data.indexOf('unavailable') !== -1) {
         reject('Not Available');
         return;
       }
 
-      data = JSON.parse(data);
-      if (data?.errors) {
-        console.log('getLocationInfo: ' + data);
-        reject('Not Available');
+      let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([01])/);
+      if (match && match.length === 3) {
+        resolve({ region: match[1], cnbl: parseInt(match[2]) });
         return;
       }
 
-      let {
-        token: { accessToken },
-        session: {
-          inSupportedLocation,
-          location: { countryCode },
-        },
-      } = data?.extensions?.sdk;
-      resolve({ inSupportedLocation, countryCode, accessToken });
+      resolve({ region: null, cnbl: 0 });
     });
   });
 }
 
 function testHomePage() {
   return new Promise((resolve, reject) => {
-    let opts = {
-      url: 'https://www.disneyplus.com/',
-      headers: {
-        'Accept-Language': 'en',
-        'User-Agent': UA,
-      },
+    let option = {
+      url: 'https://www.disneyplus.com/home',
+      headers: REQUEST_HEADERS,
     };
-
-    $httpClient.get(opts, function (error, response, data) {
+    $httpClient.get(option, function (error, response, data) {
       if (error) {
         reject('Error');
         return;
       }
-      if (response.status !== 200 || data.indexOf('Sorry, Disney+ is not available in your region.') !== -1) {
-        reject('Not Available');
+      if (response.status === 200) {
+        resolve({ status: STATUS_AVAILABLE });
         return;
       }
-
-      let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/);
-      if (!match) {
-        resolve({ region: '', cnbl: '' });
+      if (response.status === 403) {
+        resolve({ status: STATUS_NOT_AVAILABLE });
         return;
       }
-
-      let region = match[1];
-      let cnbl = match[2];
-      resolve({ region, cnbl });
+      reject('Error');
     });
   });
 }
@@ -353,18 +285,22 @@ function timeout(delay = 5000) {
 
 async function getTraceData() {
   return new Promise((resolve, reject) => {
-    $httpClient.get("http://chat.openai.com/cdn-cgi/trace", function(error, response, data) {
-      if (error) {
-        reject(error);
+    let option = {
+      url: 'http://www.cloudflare.com/cdn-cgi/trace',
+    };
+    $httpClient.get(option, function (error, response, data) {
+      if (error || response.status !== 200) {
+        reject('Error');
         return;
       }
-      let lines = data.split("\n");
-      let cf = lines.reduce((acc, line) => {
-        let [key, value] = line.split("=");
-        acc[key] = value;
-        return acc;
-      }, {});
-      resolve(cf);
+      let result = {};
+      data.split('\n').forEach((line) => {
+        let parts = line.split('=');
+        if (parts.length === 2) {
+          result[parts[0]] = parts[1];
+        }
+      });
+      resolve(result);
     });
   });
 }
