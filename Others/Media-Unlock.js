3200ddaa411e218fd68bf5bfdb02fc921bcd7948
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-02 18:45:36 +0800

    更新 Media-Unlock.js

--- Others/Media-Unlock.js
+++ Others/Media-Unlock.js
@@ -37,35 +37,35 @@ const WARP_FEATURES = ["plus", "on"];
 let args = getArgs();
 
 (async () => {
-  let now = new Date();
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-  hour = hour > 9 ? hour : "0" + hour;
-  minutes = minutes > 9 ? minutes : "0" + minutes;
+let now = new Date();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+hour = hour > 9 ? hour : "0" + hour;
+minutes = minutes > 9 ? minutes : "0" + minutes;
 
-  let panel_result = {
-    title: `${args.title} | ${hour}:${minutes}` || `解锁检测 | ${hour}:${minutes}`,
-    content: '',
-    icon: args.icon || "eye.slash.circle.fill",
-    "icon-color": args.color || "#ffb621",
-  };
+let panel_result = {
+  title: `${args.title} | ${hour}:${minutes}` || `解鎖檢測 | ${hour}:${minutes}`,
+  content: '',
+  icon: args.icon || "eye.slash.circle.fill",
+  "icon-color": args.color || "#ffb621",
+};
 
-  let [{ region, status }] = await Promise.all([testDisneyPlus()]);
-  let youtubeResult = await check_youtube_premium();
-  let netflixResult = await check_netflix();
+let [{ region, status }] = await Promise.all([testDisneyPlus()]);
+let netflixResult = await check_netflix();
+let youtubeResult = await check_youtube_premium();
 
-  let disney_result = formatDisneyPlusResult(status, region);
-  let content = `${youtube} ${netflix}\n${gptSupportStatus}${traceData.loc.padEnd(3)}${disney_result} `;
-  
-  let traceData = await getTraceData();
-  let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
+let disney_result = formatDisneyPlusResult(status, region);
+let traceData = await getTraceData();
+let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
 
-  let log = `${hour}:${minutes}.${now.getMilliseconds()} 解锁检测完成：${content}`;
-  console.log(log);
+let content = `${youtubeResult} ${netflixResult}\n${gptSupportStatus}${traceData.loc.padEnd(3)}${disney_result} `;
 
-  panel_result['content'] = content;
+let log = `${hour}:${minutes}.${now.getMilliseconds()} 解鎖檢測完成：${content}`;
+console.log(log);
 
-  $done(panel_result);
+panel_result['content'] = content;
+
+$done(panel_result);
 })();
 
 function getArgs() {
