ec037622d29927accbea801db6c25ee073162979
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-03 15:07:52 +0800

    Update test.js

--- Others/test.js
+++ Others/test.js
@@ -57,7 +57,10 @@ let disney_result = formatDisneyPlusResult(status, region);
 let traceData = await getTraceData();
 let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "ChatGPT: \u2611" : "ChatGPT: \u2612";
 
-let content = `${youtubeResult} ${netflixResult}\n${gptSupportStatus}${traceData.loc.padEnd(3)}${disney_result} `;
+let content = [
+  `${youtubeResult} \t| ${netflixResult}`,
+  `${gptSupportStatus}${traceData.loc.padEnd(3)} \t| ${disney_result}`,
+];
 
 let log = `${hour}:${minutes}.${now.getMilliseconds()} 解鎖檢測完成：${content}`;
 console.log(log);
