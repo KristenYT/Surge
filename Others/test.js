bc62d68d68937bb6c1037517f3d088e40e2372c3
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-03 15:00:30 +0800

    Update test.js

--- Others/test.js
+++ Others/test.js
@@ -131,7 +131,7 @@ async function check_youtube_premium() {
       if (code === 'Not Available') {
         youtube_check_result += '\u2612';
       } else {
-        youtube_check_result += "\u2611" + code.toUpperCase()';
+        youtube_check_result += "\u2009\u2611" + code.toUpperCase() + ' \u2009';
       }
     })
     .catch(() => {
