0f0e7ab3417484e226146e4ec407bf4e28a1c516
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-10 02:28:48 +0800

    Update Media-Unlock.js

--- Others/Media-Unlock.js
+++ Others/Media-Unlock.js
@@ -26,18 +26,18 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     }
     
     // 同时检测多个服务
-    let [{ region, status }] = await Promise.all([testDisneyPlus()])
+   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_chatgpt(), check_youtube_premium(), check_netflix()])
         .then((result) => {
         let disney_result = ''
         if (status == STATUS_COMING) {
-            disney_result = 'Disney+: 即将登陆~ ' + region
+            disney_result = 'Disney\u2009➟ Soon~ ' + region
         } else if (status == STATUS_AVAILABLE){
-            disney_result = 'Disney+: 已解锁，区域: ' + region
+            disney_result = Disney\u2009➟ \u2611\u2009' + region
         } else if (status == STATUS_NOT_AVAILABLE) {
-            disney_result = 'Disney+: 未支持 🚫'
+            disney_result = 'Disney\u2009➟ \u2612'
         } else if (status == STATUS_TIMEOUT) {
-            disney_result = 'Disney+: 检测超时 🚦'
+            disney_result = 'Disney\u2009➟ N/A '
         }
         result.push(disney_result)
 
@@ -261,4 +261,117 @@ async function testDisneyPlus() {
         return {status: STATUS_ERROR}
     }
 
+}
+
+function getLocationInfo() {
+    return new Promise((resolve, reject) => {
+        let opts = {
+            url: 'https://disney.api.edge.bamgrid.com/graph/v1/device/graphql',
+            headers: {
+                'Accept-Language': 'en',
+                Authorization: 'ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84',
+                'Content-Type': 'application/json',
+                'User-Agent': UA,
+            },
+            body: JSON.stringify({
+                query: 'mutation registerDevice($input: RegisterDeviceInput!) { registerDevice(registerDevice: $input) { grant { grantType assertion } } }',
+                variables: {
+                    input: {
+                        applicationRuntime: 'chrome',
+                        attributes: {
+                            browserName: 'chrome',
+                            browserVersion: '94.0.4606',
+                            manufacturer: 'apple',
+                            model: null,
+                            operatingSystem: 'macintosh',
+                            operatingSystemVersion: '10.15.7',
+                            osDeviceIds: [],
+                        },
+                        deviceFamily: 'browser',
+                        deviceLanguage: 'en',
+                        deviceProfile: 'macosx',
+                    },
+                },
+            }),
+        }
+
+        $httpClient.post(opts, function (error, response, data) {
+            if (error) {
+                reject('Error')
+                return
+            }
+
+            if (response.status !== 200) {
+                reject('Not Available')
+                return
+            }
+
+            data = JSON.parse(data)
+            if(data?.errors){
+                reject('Not Available')
+                return
+            }
+
+            let {
+                token: {accessToken},
+                session: {
+                    inSupportedLocation,
+                    location: {countryCode},
+                },
+            } = data?.extensions?.sdk
+            resolve({inSupportedLocation, countryCode, accessToken})
+        })
+    })
+}
+
+function testHomePage() {
+    return new Promise((resolve, reject) => {
+        let opts = {
+            url: 'https://www.disneyplus.com/',
+            headers: {
+                'Accept-Language': 'en',
+                'User-Agent': UA,
+            },
+        }
+
+        $httpClient.get(opts, function (error, response, data) {
+            if (error) {
+                reject('Error')
+                return
+            }
+            if (response.status !== 200 || data.indexOf('Sorry, Disney+ is not available in your region.') !== -1) {
+                reject('Not Available')
+                return
+            }
+
+            let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/)
+            if (!match) {
+                resolve({region: '', cnbl: ''})
+                return
+            }
+
+            let region = match[1]
+            let cnbl = match[2]
+            resolve({region, cnbl})
+        })
+    })
+}
+
+function timeout(delay = 5000) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('Timeout')
+        }, delay)
+    })
+}
+
+function getIcon(code, icons) {
+    if (code != null && code.length === 2){
+        for (let i = 0; i < icons.length; i++) {
+            if (icons[i][0] === code) {
+                return icons[i][1] + code
+            }
+        }
+    }
+    return code
 }
