0143f13a3d42947066223be318127a9297c97683
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-10 06:33:33 +0800

    更新 Media-Unlock.js

--- Others/Media-Unlock.js
+++ Others/Media-Unlock.js
@@ -22,6 +22,7 @@ const STATUS_ERROR = -2
 
 const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
 
+    let args = $argument
 ;(async (args) => {
     let now = new Date();
     let hour = now.getHours();
