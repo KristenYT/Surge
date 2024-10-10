6643084e04c5b9fa575b97d40d74df92773c867e
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-10 12:37:47 +0800

    更新 Media-Unlock.js

--- Others/Media-Unlock.js
+++ Others/Media-Unlock.js
@@ -43,7 +43,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
         .then((result) => {
         let disney_result = ''
         if (status == STATUS_COMING) {
-            disney_result = 'Disney\u2009➟ ≈ ' + region
+            disney_result = 'Disney\u2009➟ \u2009≈ ' + region
         } else if (status == STATUS_AVAILABLE){
             disney_result = 'Disney\u2009➟ \u2611\u2009' + region
         } else if (status == STATUS_NOT_AVAILABLE) {
