ba00f4c2d6cd198da76024f7aae6c21b4512ec7b
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-12 03:04:41 +0800

    Update Youtube-SUB-fix.js

--- Script/Youtube-SUB-fix.js
+++ Script/Youtube-SUB-fix.js
@@ -17,13 +17,36 @@ $httpClient.get('https://raw.githubusercontent.com/KristenYT/Surge/refs/heads/ma
             // 獲取字幕內容
             let responseBody = $response.body;
 
-            // 使用 OpenCC 進行簡體轉繁體
-            converter.convertPromise(responseBody).then((traditionalText) => {
-                $done({ body: traditionalText });
-            }).catch((conversionError) => {
-                console.error("轉換錯誤：", conversionError);
-                $done({ body: responseBody });
-            });
+            // 解析 JSON 格式的字幕
+            let parsedBody;
+            try {
+                parsedBody = JSON.parse(responseBody);
+            } catch (jsonError) {
+                console.error("字幕內容不是 JSON 格式，將按文本進行轉換");
+                parsedBody = null;
+            }
+
+            if (parsedBody) {
+                // 對字幕文本進行處理
+                for (let i = 0; i < parsedBody.length; i++) {
+                    if (parsedBody[i].segs) {
+                        for (let j = 0; j < parsedBody[i].segs.length; j++) {
+                            parsedBody[i].segs[j].utf8 = converter.convert(parsedBody[i].segs[j].utf8);
+                        }
+                    }
+                }
+
+                // 返回轉換後的 JSON
+                $done({ body: JSON.stringify(parsedBody) });
+            } else {
+                // 如果不是 JSON，將純文本進行轉換
+                converter.convertPromise(responseBody).then((traditionalText) => {
+                    $done({ body: traditionalText });
+                }).catch((conversionError) => {
+                    console.error("轉換錯誤：", conversionError);
+                    $done({ body: responseBody });
+                });
+            }
         } else {
             console.error("OpenCC 未正確加載");
             $done({ body: $response.body });
