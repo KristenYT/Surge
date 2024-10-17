2b38ca32a685eca8210f29f5571876f800c55aa8
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-18 03:51:21 +0800

    Update Follow.js

--- Script/Follow.js
+++ Script/Follow.js
@@ -1,7 +1,3 @@
-/*
-作者:可莉
-脚本引用 https://github.com/chavyleung/scripts/blob/master/follow/follow.js , https://raw.githubusercontent.com/luestr/ProxyResource/main/Resource/Script/FollowRSS/FollowRSS_checkin.js
-*/
 const $ = new Env('Follow每日签到')
 $.desc = []
 
