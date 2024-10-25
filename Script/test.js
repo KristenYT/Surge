10327c0490bf8874faebf38040032327920cf4f3
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-25 17:34:07 +0800

    更新 test.js

--- Script/test.js
+++ Script/test.js
@@ -1,5 +1,5 @@
 /**
- * 更新日期：2024-04-05 15:30:15
+ * 更新日期：2024-10-25
  * 用法：Sub-Store 脚本操作添加
  * rename.js 以下是此脚本支持的参数，必须以 # 为开头多个参数使用"&"连接，参考上述地址为例使用参数。 禁用缓存url#noCache
  *
@@ -12,7 +12,6 @@
  * [in=en] 或in=us 识别英文缩写
  * [in=flag] 或in=gq 识别国旗 如果加参数 in=flag 则识别国旗 脚本操作前面不要添加国旗操作 否则移除国旗后面脚本识别不到
  * [in=quan] 识别英文全称
-
  *
  * [out=]   输出节点名可选参数: (cn或zh ，us或en ，gq或flag ，quan) 对应：(中文，英文缩写 ，国旗 ，英文全称) 默认中文 例如 [out=en] 或 out=us 输出英文缩写
  *** 分隔符参数
@@ -75,7 +74,7 @@ const FG = ['🇭🇰','🇲🇴','🇹🇼','🇯🇵','🇰🇷','🇸🇬','�
 // prettier-ignore
 const EN = ['HK','MO','TW','JP','KR','SG','US','GB','FR','DE','AU','AE','AF','AL','DZ','AO','AR','AM','AT','AZ','BH','BD','BY','BE','BZ','BJ','BT','BO','BA','BW','BR','VG','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CO','KM','CG','CD','CR','HR','CY','CZ','DK','DJ','DO','EC','EG','SV','GQ','ER','EE','ET','FJ','FI','GA','GM','GE','GH','GR','GL','GT','GN','GY','HT','HN','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','CI','JM','JO','KZ','KE','KW','KG','LA','LV','LB','LS','LR','LY','LT','LU','MK','MG','MW','MY','MV','ML','MT','MR','MU','MX','MD','MC','MN','ME','MA','MZ','MM','NA','NP','NL','NZ','NI','NE','NG','KP','NO','OM','PK','PA','PY','PE','PH','PT','PR','QA','RO','RU','RW','SM','SA','SN','RS','SL','SK','SI','SO','ZA','ES','LK','SD','SR','SZ','SE','CH','SY','TJ','TZ','TH','TG','TO','TT','TN','TR','TM','VI','UG','UA','UY','UZ','VE','VN','YE','ZM','ZW','AD','RE','PL','GU','VA','LI','CW','SC','AQ','GI','CU','FO','AX','BM','TL'];
 // prettier-ignore
-const ZH = ['香港','澳門','台灣','日本','韩国','新加坡','美國','英国','法国','德国','澳大利亚','阿联酋','阿富汗','阿尔巴尼亚','阿尔及利亚','安哥拉','阿根廷','亚美尼亚','奥地利','阿塞拜疆','巴林','孟加拉国','白俄罗斯','比利时','伯利兹','贝宁','不丹','玻利维亚','波斯尼亚和黑塞哥维那','博茨瓦纳','巴西','英属维京群岛','文莱','保加利亚','布基纳法索','布隆迪','柬埔寨','喀麦隆','加拿大','佛得角','开曼群岛','中非共和国','乍得','智利','哥伦比亚','科摩罗','刚果(布)','刚果(金)','哥斯达黎加','克罗地亚','塞浦路斯','捷克','丹麦','吉布提','多米尼加共和国','厄瓜多尔','埃及','萨尔瓦多','赤道几内亚','厄立特里亚','爱沙尼亚','埃塞俄比亚','斐济','芬兰','加蓬','冈比亚','格鲁吉亚','加纳','希腊','格陵兰','危地马拉','几内亚','圭亚那','海地','洪都拉斯','匈牙利','冰岛','印度','印尼','伊朗','伊拉克','爱尔兰','马恩岛','以色列','意大利','科特迪瓦','牙买加','约旦','哈萨克斯坦','肯尼亚','科威特','吉尔吉斯斯坦','老挝','拉脱维亚','黎巴嫩','莱索托','利比里亚','利比亚','立陶宛','卢森堡','马其顿','马达加斯加','马拉维','马来','马尔代夫','马里','马耳他','毛利塔尼亚','毛里求斯','墨西哥','摩尔多瓦','摩纳哥','蒙古','黑山共和国','摩洛哥','莫桑比克','缅甸','纳米比亚','尼泊尔','荷兰','新西兰','尼加拉瓜','尼日尔','尼日利亚','朝鲜','挪威','阿曼','巴基斯坦','巴拿马','巴拉圭','秘鲁','菲律宾','葡萄牙','波多黎各','卡塔尔','罗马尼亚','俄罗斯','卢旺达','圣马力诺','沙特阿拉伯','塞内加尔','塞尔维亚','塞拉利昂','斯洛伐克','斯洛文尼亚','索马里','南非','西班牙','斯里兰卡','苏丹','苏里南','斯威士兰','瑞典','瑞士','叙利亚','塔吉克斯坦','坦桑尼亚','泰国','多哥','汤加','特立尼達和多巴哥','突尼斯','土耳其','土庫曼斯坦','美屬維爾京群島','烏乾達','烏克蘭','烏拉圭','烏茲別克斯坦','委內瑞拉','越南','也門','贊比亞','津巴布韋','安道爾','留尼汪','波蘭','關島','梵蒂岡','列支敦士登','庫拉索','塞舌爾','南極','直布羅陀','古巴','法羅群島','奧蘭群島','百慕達','東帝汶'];
+const ZH = ['香港', '澳門', '台灣', '日本', '韓國', '新加坡', '美國', '英國', '法國', '德國', '澳大利亞', '阿聯酋', '阿富汗', '阿爾巴尼亞', '阿爾及利亞', '安哥拉', '阿根廷', '亞美尼亞', '奧地利', '阿塞拜疆', '巴林', '孟加拉', '白俄羅斯', '比利時', '伯利茲', '貝南', '不丹', '玻利維亞', '波斯尼亞和黑塞哥維那', '博茨瓦納', '巴西', '英屬維京群島', '汶萊', '保加利亞', '布基納法索', '布隆迪', '柬埔寨', '喀麥隆', '加拿大', '佛得角', '開曼群島', '中非共和國', '乍得', '智利', '哥倫比亞', '科摩羅', '剛果(布)', '剛果(金)', '哥斯達黎加', '克羅地亞', '塞浦路斯', '捷克', '丹麥', '吉布提', '多明尼加共和國', '厄瓜多', '埃及', '薩爾瓦多', '赤道幾內亞', '厄立特里亞', '愛沙尼亞', '埃塞俄比亞', '斐濟', '芬蘭', '加蓬', '甘比亞', '格魯吉亞', '迦納', '希臘', '格陵蘭', '危地馬拉', '幾內亞', '圭亞那', '海地', '宏都拉斯', '匈牙利', '冰島', '印度', '印尼', '伊朗', '伊拉克', '愛爾蘭', '馬恩島', '以色列', '義大利', '象牙海岸', '牙買加', '約旦', '哈薩克', '肯亞', '科威特', '吉爾吉斯', '寮國', '拉脫維亞', '黎巴嫩', '賴索托', '賴比瑞亞', '利比亞', '立陶宛', '盧森堡', '北馬其頓', '馬達加斯加', '馬拉威', '馬來西亞', '馬爾地夫', '馬利', '馬爾他', '茅利塔尼亞', '模里西斯', '墨西哥', '摩爾多瓦', '摩納哥', '蒙古', '蒙特內哥羅', '摩洛哥', '莫三比克', '緬甸', '納米比亞', '尼泊爾', '荷蘭', '紐西蘭', '尼加拉瓜', '尼日', '奈及利亞', '北韓', '挪威', '阿曼', '巴基斯坦', '巴拿馬', '巴拉圭', '秘魯', '菲律賓', '葡萄牙', '波多黎各', '卡達', '羅馬尼亞', '俄羅斯', '盧安達', '聖馬利諾', '沙烏地阿拉伯', '塞內加爾', '塞爾維亞', '獅子山', '斯洛伐克', '斯洛維尼亞', '索馬利亞', '南非', '西班牙', '斯里蘭卡', '蘇丹', '蘇利南', '史瓦帝尼', '瑞典', '瑞士', '敘利亞', '塔吉克', '坦尚尼亞', '泰國', '多哥', '東加', '千里達及托巴哥', '突尼西亞', '土耳其', '土庫曼', '美屬維京群島', '烏干達', '烏克蘭', '烏拉圭', '烏茲別克', '委內瑞拉', '越南', '葉門', '尚比亞', '辛巴威', '安道爾', '留尼旺', '波蘭', '關島', '梵蒂岡', '列支敦斯登', '庫拉索', '塞席爾', '南極', '直布羅陀', '古巴', '法羅群島', '奧蘭群島', '百慕達', '東帝汶'];
 // prettier-ignore
 const QC = ['Hong Kong','Macao','Taiwan','Japan','Korea','Singapore','United States','United Kingdom','France','Germany','Australia','Dubai','Afghanistan','Albania','Algeria','Angola','Argentina','Armenia','Austria','Azerbaijan','Bahrain','Bangladesh','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','British Virgin Islands','Brunei','Bulgaria','Burkina-faso','Burundi','Cambodia','Cameroon','Canada','CapeVerde','CaymanIslands','Central African Republic','Chad','Chile','Colombia','Comoros','Congo-Brazzaville','Congo-Kinshasa','CostaRica','Croatia','Cyprus','Czech Republic','Denmark','Djibouti','Dominican Republic','Ecuador','Egypt','EISalvador','Equatorial Guinea','Eritrea','Estonia','Ethiopia','Fiji','Finland','Gabon','Gambia','Georgia','Ghana','Greece','Greenland','Guatemala','Guinea','Guyana','Haiti','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Isle of Man','Israel','Italy','Ivory Coast','Jamaica','Jordan','Kazakstan','Kenya','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Lithuania','Luxembourg','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Mauritania','Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Morocco','Mozambique','Myanmar(Burma)','Namibia','Nepal','Netherlands','New Zealand','Nicaragua','Niger','Nigeria','NorthKorea','Norway','Oman','Pakistan','Panama','Paraguay','Peru','Philippines','Portugal','PuertoRico','Qatar','Romania','Russia','Rwanda','SanMarino','SaudiArabia','Senegal','Serbia','SierraLeone','Slovakia','Slovenia','Somalia','SouthAfrica','Spain','SriLanka','Sudan','Suriname','Swaziland','Sweden','Switzerland','Syria','Tajikstan','Tanzania','Thailand','Togo','Tonga','TrinidadandTobago','Tunisia','Turkey','Turkmenistan','U.S.Virgin Islands','Uganda','Ukraine','Uruguay','Uzbekistan','Venezuela','Vietnam','Yemen','Zambia','Zimbabwe','Andorra','Reunion','Poland','Guam','Vatican','Liechtensteins','Curacao','Seychelles','Antarctica','Gibraltar','Cuba','Faroe Islands','Ahvenanmaa','Bermuda','Timor-Leste'];
 const specialRegex = [
@@ -83,7 +82,7 @@ const specialRegex = [
   /IPLC|IEPL|Kern|Edge|Pro|Std|Exp|Biz|Fam|Game|Buy|Zx|LB|Game/,
 ];
 const nameclear =
-  /(套餐|到期|有效|剩余|版本|已用|过期|失联|测试|官方|网址|备用|群|TEST|客服|网站|获取|订阅|流量|机场|下次|官址|联系|邮箱|工单|学术|USE|USED|TOTAL|EXPIRE|EMAIL)/i;
+  /(官方|网址|群|客服|网站|获取|下次|官址|联系|邮箱|工单|学术|USE|USED|EMAIL)/i;
 // prettier-ignore
 const regexArray=[/ˣ²/, /ˣ³/, /ˣ⁴/, /ˣ⁵/, /ˣ⁶/, /ˣ⁷/, /ˣ⁸/, /ˣ⁹/, /ˣ¹⁰/, /ˣ²⁰/, /ˣ³⁰/, /ˣ⁴⁰/, /ˣ⁵⁰/, /IPLC/i, /IEPL/i, /核心/, /边缘/, /高级/, /标准/, /实验/, /商宽/, /家宽/, /游戏|game/i, /购物/, /专线/, /LB/, /cloudflare/i, /\budp\b/i, /\bgpt\b/i,/udpn\b/];
 // prettier-ignore
@@ -109,7 +108,7 @@ const rurekey = {
   香港: /(深|沪|呼|京|广|杭)港(?!.*(I|线))/g,
   日本: /(深|沪|呼|京|广|杭|中|辽)日(?!.*(I|线))|东京|大坂/g,
   新加坡: /狮城|(深|沪|呼|京|广|杭)新/g,
-  美国: /(深|沪|呼|京|广|杭)美|波特兰|芝加哥|哥伦布|纽约|硅谷|俄勒冈|西雅图|芝加哥/g,
+  美國: /(深|沪|呼|京|广|杭)美|波特兰|芝加哥|哥伦布|纽约|硅谷|俄勒冈|西雅图|芝加哥/g,
   波斯尼亚和黑塞哥维那: /波黑共和国/g,
   印尼: /印度尼西亚|雅加达/g,
   印度: /孟买/g,
@@ -124,7 +123,7 @@ const rurekey = {
   India: /Mumbai/g,
   Germany: /Frankfurt/g,
   Switzerland: /Zurich/g,
-  俄罗斯: /莫斯科/g,
+  俄羅斯: /莫斯科/g,
   土耳其: /伊斯坦布尔/g,
   泰国: /泰國|曼谷/g,
   法国: /巴黎/g,
@@ -271,7 +270,7 @@ function operator(pro) {
         const index = outList.indexOf(findKeyValue);
         if (index !== -1) {
           usflag = FG[index];
-          usflag = usflag === "🇹🇼" ? "🇹🇼" : usflag;
+          usflag = usflag === "🇹🇼" ? "🇨🇳" : usflag;
         }
       }
       keyover = keyover
