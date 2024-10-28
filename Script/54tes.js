a13dbf0ac2565c73da85e392e05b1e9025ed7b06
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-29 07:40:00 +0800

    Update and rename 53tes.js to 54tes.js

--- Script/54tes.js
+++ Script/54tes.js
@@ -280,17 +280,18 @@ function operator(pro) {
     } else {
       nNames = FNAME;
     }
-    if (findKey?.[1]) {
-      const findKeyValue = findKey[1];
-      let keyover = [],
+    if (findKey?.[1] && out === "zh") {
+    const findKeyValue = findKey[1];
+    let keyover = [],
         usflag = "";
-      if (addflag) {
+    if (addflag) {
         const index = outList.indexOf(findKeyValue);
         if (index !== -1) {
-          usflag = FG[index];
-          usflag = usflag === "🇹🇼" ? "🇹🇼" : usflag;
+            usflag = FG[index];
+            usflag = usflag === "🇹🇼" ? "🇼🇸" : usflag;
         }
-      }
+    }
+}
       keyover = keyover
         .concat(firstName, usflag, findKeyValue, retainKey, ikey, ikeys)
         .filter((k) => k !== "");
