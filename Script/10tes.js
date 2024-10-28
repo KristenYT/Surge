d77dbe67a986ab5998b626d18c10ac47ccf99241
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-29 01:13:43 +0800

    Update and rename 9tes.js to 10tes.js

--- Script/10tes.js
+++ Script/10tes.js
@@ -303,9 +303,6 @@ function operator(pro) {
   } else {
     e.name = null; // 當 nm 為 false 時，將 e.name 設為 null
   }
-}
-e = null; // 當 nm 為 false 時，將 e.name 設為 null
-}
 
   pro = pro.filter((e) => e.name !== null);
   jxh(pro);
@@ -318,9 +315,26 @@ e = null; // 當 nm 為 false 時，將 e.name 設為 null
 // prettier-ignore
 function getList(arg) { switch (arg) { case 'zht': return ZHT;case 'us': return EN; case 'gq': return FG; case 'quan': return QC; default: return ZH; }}
 // prettier-ignore
-function jxh(e) { const n = e.reduce((e, n) => { const t = e.find((e) => e.name === n.name); if (t) { t.count++; t.items.push({ ...n, name: `${t.count.toString().padStart(2, "0")}${FNAME}`
-, }); } else { e.push({ name: n.name, count: 1, items: [{ ...n, name: `${n.name} 01 ${FNAME}`
- }], }); } return e; }, []);const t=(typeof Array.prototype.flatMap==='function'?n.flatMap((e) => e.items):n.reduce((acc, e) => acc.concat(e.items),[])); e.splice(0, e.length, ...t); return e;}
+function jxh(e) { 
+  const n = e.reduce((e, n) => { 
+    const t = e.find((e) => e.name === n.name); 
+    if (t) { 
+      t.count++; 
+      t.items.push({ ...n, name: `${n.name} ${t.count.toString().padStart(2, "0")}${FNAME}` }); 
+    } else { 
+      e.push({ name: n.name, count: 1, items: [{ ...n, name: `${n.name} 01${FNAME}` }] }); 
+    } 
+    return e; 
+  }, []);
+
+  const t = (typeof Array.prototype.flatMap === 'function'
+    ? n.flatMap((e) => e.items)
+    : n.reduce((acc, e) => acc.concat(e.items), [])); 
+
+  e.splice(0, e.length, ...t); 
+  return e;
+}
+
 // prettier-ignore
 function oneP(e) { const t = e.reduce((e, t) => { const n = t.name.replace(/[^A-Za-z0-9\u00C0-\u017F\u4E00-\u9FFF]+\d+$/, ""); if (!e[n]) { e[n] = []; } e[n].push(t); return e; }, {}); for (const e in t) { if (t[e].length === 1 && t[e][0].name.endsWith("01")) {/* const n = t[e][0]; n.name = e;*/ t[e][0].name= t[e][0].name.replace(/[^.]01/, "") } } return e; }
 // prettier-ignore
