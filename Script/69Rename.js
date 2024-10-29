003376d1fcc5c4491d449d6988b645fa1c27bfa5
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-10-30 01:59:13 +0800

    Update and rename 68Rename.js to 69Rename.js

--- Script/69Rename.js
+++ Script/69Rename.js
@@ -343,7 +343,7 @@ function jxh(e) {
       existingGroup.count++;
       existingGroup.items.push({
         ...currentItem,
-        name: `${currentItem.name} ${toSuperscript(existingGroup.count.toString().padStart(2, "0"))} ${BLKEYS} ${FNAME}`
+        name: `${currentItem.name} ${toSuperscript(existingGroup.count.toString().padStart(2, "0"))} ${BLKEY_REPLACE} ${FNAME}`
       });
     } else {
       acc.push({
@@ -351,7 +351,7 @@ function jxh(e) {
         count: 1,
         items: [{
           ...currentItem,
-          name: `${currentItem.name} ${BLKEYS} ${FNAME}`
+          name: `${currentItem.name} ${BLKEY_REPLACE} ${FNAME}`
         }],
       });
     }
@@ -362,7 +362,7 @@ function jxh(e) {
   groups.forEach(group => {
     if (group.count > 1) {
       // 更新第一个元素的名称以包含序号“01”
-      group.items[0].name = `${group.name} ${toSuperscript("01")} ${BLKEYS} ${FNAME}`;
+      group.items[0].name = `${group.name} ${toSuperscript("01")} ${BLKEY_REPLACE} ${FNAME}`;
     }
   });
 
