cd3e17f74dea87a57101aecc1ef4fcbaa680f847
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-11-01 05:18:48 +0800

    Update and rename 1279.js to 127889.js

--- Script/127889.js
+++ Script/127889.js
@@ -229,8 +229,8 @@ pro.forEach((e) => {
         });
 
         retainKey = re
-          ? "-" + BLKEY_REPLACE
-          : "-" + BLKEYS.filter((items) => e.name.includes(items));
+          ? BLKEY_REPLACE
+          : BLKEYS.filter((items) => e.name.includes(items));
       }
     }
   });
@@ -258,8 +258,8 @@ pro.forEach((e) => {
     });
 
     retainKey = re
-      ? BLKEY_REPLACE
-      : BLKEYS.filter((items) => e.name.includes(items));
+      ? "-" + BLKEY_REPLACE
+      : "-" + BLKEYS.filter((items) => e.name.includes(items));
   }
 
   let ikey = "",
