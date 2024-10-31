422fa1104f66c7a35588457e2ba9a99e6857567c
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-11-01 05:24:46 +0800

    Update and rename 12799.js to 12798.js

--- Script/12798.js
+++ Script/12798.js
@@ -230,7 +230,7 @@ pro.forEach((e) => {
 
         retainKey = re
           ? BLKEY_REPLACE
-          : "-" + BLKEYS.filter((items) => e.name.includes(items));
+          : BLKEYS.filter((items) => e.name.includes(items));
       }
     }
   });
