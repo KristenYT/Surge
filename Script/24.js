60c86911da4415216f9e71563e2bd833d03133b3
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-11-03 01:38:51 +0800

    Update and rename 23.js to 24.js

--- Script/24.js
+++ Script/24.js
@@ -359,7 +359,7 @@ function jxh(e) {
       existingGroup.count++;
       existingGroup.items.push({
         ...currentItem,
-        name: `${currentItem.name} ${toSuperscript(existingGroup.count.toString().padStart(2, "0"))} ${BLKEY_REPLACE : BLKEYS.filter((items) => e.name.includes(items))} ${FNAME}`
+        name: `${currentItem.name} ${toSuperscript(existingGroup.count.toString().padStart(2, "0"))} ${retainKey} ${FNAME}`
       });
     } else {
       acc.push({
@@ -367,7 +367,7 @@ function jxh(e) {
         count: 1,
         items: [{
           ...currentItem,
-          name: `${currentItem.name} ${BLKEY_REPLACE : BLKEYS.filter((items) => e.name.includes(items))} ${FNAME}`
+          name: `${currentItem.name} ${retainKey} ${FNAME}`
         }],
       });
     }
@@ -378,7 +378,7 @@ function jxh(e) {
   groups.forEach(group => {
     if (group.count > 1) {
       // 更新第一个元素的名称以包含序号“01”
-      group.items[0].name = `${group.name} ${toSuperscript("01")} ${BLKEY_REPLACE : BLKEYS.filter((items) => e.name.includes(items))}`;
+      group.items[0].name = `${group.name} ${toSuperscript("01")} ${retainKey} ${FNAME}`;
     }
   });
 
