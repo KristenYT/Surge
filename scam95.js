629aa1552e7371c1437fb1f8416ce53f7fae999f
Kristen <106135199+KristenYT@users.noreply.github.com>
2024-11-22 02:24:44 +0800

    Update and rename scam94.js to scam95.js

--- scam95.js
+++ scam95.js
@@ -1,5 +1,7 @@
 // 獲取節點名稱的語法
-const nodeName = $environment?.params || "未知節點";
+let arg = $argument || "";
+let args = Object.fromEntries(new URLSearchParams(arg).entries());
+let nodeName = args.nodeName || "未知節點";
 
 // 第一步：獲取外部 IP 地址信息
 $httpClient.get({ url: "http://ip-api.com/json/" }, function (error, response, data) {
